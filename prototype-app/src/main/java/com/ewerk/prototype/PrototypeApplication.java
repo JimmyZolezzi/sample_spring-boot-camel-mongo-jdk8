cd0386f51832a7b0c5c7c0e8c70bae788b5723c9
Holger Stolzenberg <h.stolzenberg@ewerk.com>
2014-09-13 17:08:44 +0200

    The prototype-app now manages a PID file.

--- prototype-app/src/main/java/com/ewerk/prototype/PrototypeApplication.java
+++ prototype-app/src/main/java/com/ewerk/prototype/PrototypeApplication.java
@@ -3,6 +3,7 @@ package com.ewerk.prototype;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidListener;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -11,23 +12,30 @@ import org.springframework.context.annotation.ComponentScan;
  * application form CLI oder IDE. In the Spring samples this class is often annotated with {@link
  * org.springframework.context.annotation.Configuration} and is therefore used as bean factory. We
  * do not go this way in this example, as we want to use more purpose specific configuration
- * classes.
+ * classes.<br/><br/>
+ *
+ * The app registers a {@link org.springframework.boot.actuate.system.ApplicationPidListener} and
+ * writes a PID file {@link PrototypeApplication#PID_FILE} to the current directory. The path is
+ * currently not configurable through spring-boot itself.
  *
  * @author h.stolzenberg
  * @since 0.0.1
  */
 @ComponentScan
 @EnableAutoConfiguration
-public class Application extends SpringApplication {
+public final class PrototypeApplication {
 
   private static final Logger LOG = LoggerFactory.getLogger(LoggerFactory.class);
 
+  private static final String PID_FILE = "prototype.pid";
+
   public static void main(String[] args) {
-    SpringApplication application = new SpringApplication(Application.class);
+    SpringApplication application = new SpringApplication(PrototypeApplication.class);
     application.setHeadless(true);
     application.setRegisterShutdownHook(true);
     application.setLogStartupInfo(true);
     application.setWebEnvironment(true);
+    application.addListeners(new ApplicationPidListener(PID_FILE));
     application.run(args);
 
     LOG.info("Prototype launched [OK]");
