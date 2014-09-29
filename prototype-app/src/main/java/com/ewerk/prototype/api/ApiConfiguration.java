24897cc1e3828087a2b3107d36360c87b76574d5
Holger Stolzenberg <h.stolzenberg@ewerk.com>
2014-09-29 15:32:17 +0200

    Removed @EnableWebMvc as it is not needed. This also temporarily fixes spring-boot issue gh-1620.

--- prototype-app/src/main/java/com/ewerk/prototype/api/ApiConfiguration.java
+++ prototype-app/src/main/java/com/ewerk/prototype/api/ApiConfiguration.java
@@ -1,13 +1,7 @@
 package com.ewerk.prototype.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.List;
 
 /**
  * This bean factory enables the Spring WebMVC features of spring-boot.
@@ -15,16 +9,6 @@ import java.util.List;
  * @author h.stolzenberg
  * @since 0.0.4
  */
-@EnableWebMvc
 @Configuration
 public class ApiConfiguration extends WebMvcConfigurerAdapter {
-
-  @SuppressWarnings("SpringJavaAutowiringInspection")
-  @Autowired
-  private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter;
-
-  @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    converters.add(mappingJackson2HttpMessageConverter);
-  }
 }
